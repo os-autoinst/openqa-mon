da70f241ec4285e5e6ec3b39f0390b044c463648
phoenix <felix.niederwanger@suse.com>
2023-03-30 15:41:46 +0200

    Upgrade to gopenqa 0.7.1 and align version number
    Update to gopenqa 0.7.1 for fixing the missing links in GetFollowJobs
    and unify the version number across openqa-mon, openqa-mq and
    openqa-revtui.

--- cmd/openqa-revtui/openqa-revtui.go
+++ cmd/openqa-revtui/openqa-revtui.go
@@ -11,7 +11,7 @@ import (
 	"github.com/grisu48/gopenqa"
 )
 
-const VERSION = "0.6.0"
+const VERSION = "1.0.1"
 
 /* Group is a single configurable monitoring unit. A group contains all parameters that will be queried from openQA */
 type Group struct {
